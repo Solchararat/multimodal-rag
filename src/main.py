244115387dc6d8a2a9128458c4380247b173002e
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-05-11 11:45:54 +0800

    feat: Implement FastAPI endpoint

--- src/main.py
+++ src/main.py
@@ -1,15 +1,37 @@
-import json
 from plant_classifier import PlantClassifier
 from pathlib import Path
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+import base64
+from PIL import Image
+from io import BytesIO
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
 
-if __name__ == "__main__":
+class ImageRequest(BaseModel):
+    image: str
+
+db_path = Path(__file__).parent.parent / "db"
+plant_classifier = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     db_path = Path(__file__).parent.parent / "db"
-    
-    plant_classifier = PlantClassifier(db_path=db_path)
-    
-    # testing local image
-    print("\n=== Testing with local image ===")
-    local_image_path = "dataset/sample-images/1000_F_175913717_wh9WZV4aT5QAPnJ.jpg"
-    result_local, similar_local = plant_classifier.classify_plant(local_image_path)
-    print("Classification from local file:")
-    print(json.dumps(result_local, indent=2))
+    app.state.plant_classifier = PlantClassifier(db_path=db_path)
+    yield
+    del app.state.plant_classifier
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/classify")
+async def classify_plant(request: ImageRequest):
+    try:
+
+        
+        image_bytes = base64.b64decode(request.image)
+        image = Image.open(BytesIO(image_bytes))
+        
+        result, _ = plant_classifier.classify_plant(image)
+        return JSONResponse(content=result)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
